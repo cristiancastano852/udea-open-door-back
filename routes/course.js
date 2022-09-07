f2e3f502e45cddca8439b248f136610868380feb
Cristian Castaño <cristian.castano1@udea.edu.co>
2022-09-07 17:43:42 -0500

    update /detail/ userId for params 2

--- routes/course.js
+++ routes/course.js
@@ -4,66 +4,11 @@ import { PrismaClient } from '@prisma/client';
 const cursoRoutes = express.Router();
 const prisma = new PrismaClient();
 
-/** 
- * @swagger
- * tags:
- *  name: Course
- *  description: Endpoint to get a course
- */
-/**
- * @swagger
- * paths:
- *  /course/:courseId:
- *   get:
- *      body:
- *          - in: body            
- *            name: courseId
- *            schema:
- *              type: string
- *            description: The id of the course to get
- *          - in: body
- *            name: userId
- *            schema:
- *              type: string
- *            description: The id of the user 
- *      summary: Get a specific course with all attributes and content
- *      tags: [Course]
- *      responses:
- *          200:
- *              description: It was possible to connect to the database and obtain the course           
- *              content:    
- *                  application/json:
- *                      schema:
- *                          properties:
- *                              status:
- *                                  type: string
- *                                  example: AbleToStart
- *                              title:
- *                                  type: string
- *                                  example: Habilidades para la vida
- *                              description:
- *                                  type: string
- *                                  example: Este curso enseña sobre habilidades para la vida
- *                              courseContents:
- *                                  type: json
- *                                  example:
- *                                      name: Habilidades para la vida
- *                                      description: Documento que tiene las habilidades para la vida
- *                                      contentType: resource
- *                                      url: https://drive.google.com/uc?id=1K-xVrXnuQAbdELzzq1c7Y_PKECnImm5a&export=download
- *          
- *          204:
- *              description: No course with that id for the user id, doesn't need to navigate away from its current page
- *              
- *          500:
- *              description: There was an unexpected error reaching connecting to the database
- *          
- */
-
 cursoRoutes.route('/course/detail/:courseId').get(course());
 
 function course() {
     return async (req, res) => {
+        console.log("probandooooo1");
         try {
             console.log("probandooooo1");
             const courseId = req.params.courseId;
