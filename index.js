34c1b163a6798a653dec9b767846aa54d1269870
Victor013001 <victormanuelosoriog@gmail.com>
2022-09-07 13:03:23 -0500

    Updates

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({
-    message: 'This is API for courses',
+    message: 'This is API for Open-door',
     status: 'OK'
   })
 })
