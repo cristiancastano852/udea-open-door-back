b7716e694e2b450d7e79e53f575d6434ca250b87
Victor013001 <victormanuelosoriog@gmail.com>
2022-10-25 09:29:18 -0500

    Usercreate with all atributes

--- routes/userCreate.js
+++ routes/userCreate.js
@@ -25,6 +25,36 @@ const prisma = new PrismaClient();
  *               type: string
  *               example: userEmail@email.com
  *            description: The email of the user to create
+ *          - in: body
+ *            name: name
+ *            schema:
+ *              type: string
+ *            description: The name of the user
+ *          - in: body
+ *            name: about
+ *            schema:
+ *              type: string
+ *            description: A short description about the user
+ *          - in: body
+ *            name: age
+ *            schema:
+ *              type: int
+ *            description: The age of the user
+ *          - in: body
+ *            name: avatar
+ *            schema:
+ *              type: string
+ *            description: The link to the avatar of the user
+ *          - in: body
+ *            name: expectations
+ *            schema:
+ *              type: string
+ *            description: The expectations of the user
+ *          - in: body
+ *            name: linkedin
+ *            schema:
+ *              type: string
+ *            description: The link to the linkedin of the user
  *      summary: When you sign up you will be added as a user
  *      tags: [Create User]
  *      responses:
@@ -51,16 +81,16 @@ function createUser() {
     return async (req, res) => {
 
         try {
-            const userEmail = req.body.userEmail;
+            const { userEmail, name, avatar, about, expectations, linkedin, age } = req.body;
             const user = await prisma.User.create({
                 data: {
                     email: userEmail,
-                    name: "",
-                    about: "",
-                    age: 0,
-                    expectations: "",
-                    linkedin: "",
-                    avatar: ""
+                    name: name,
+                    about: about,
+                    age: Number(age),
+                    expectations: expectations,
+                    linkedin: linkedin,
+                    avatar: avatar
                 },
             });
             const userId = user.id;
